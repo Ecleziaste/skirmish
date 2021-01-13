c13a57ec13842342ef57e73709a77d01a4554e18
Andrew <65909640+Ecleziaste@users.noreply.github.com>
2021-01-14 00:47:26 +0600

    some propterties in objects added

--- scripts/objects.js
+++ scripts/objects.js
@@ -12,13 +12,23 @@
 
 function Humanoid ( name ) {
     this.name = name;
+    this.move = 4;
+       // кол-во ходов по карте. отнимается за передвижение и действия
+    this.weaponCapacity = 0;
+    // weaponCapacity = max 10
+    // малогабаритное занимает 3 очка, среднее 4 очка, крупное 6(7) очков
+    // 3 малогабаритных вида оружия,
+    // 2 малогабаритных вида оружия и 1 среднегабаритное, 
+    // 2 среднегабаритных, 
+    // 1 малогабаритное и 1 крупногабаритное.
+   
 
-    this.totalHealth = 20;
+    this.totalHealth = 30;
     this.isAlive = true;
     this.checkCondition = function ( hp ) {
         this.totalHealth += hp;
-        if ( this.totalHealth > 20 ) {
-            this.totalHealth = 20;
+        if ( this.totalHealth > 30 ) {
+            this.totalHealth = 30;
             return this.totalHealth;
         }
         if ( this.totalHealth === 0 ) {
@@ -31,15 +41,18 @@ function Humanoid ( name ) {
     this.heavyWounds = 0;
     
 
+    // TODO: когда ранг повышается this.totalHealth += 5; максимальное здоровье повышается на 5 (и восстанавливается здоровье до текущей величины)
     this.expirience = 0;
     this.rank = "rookie";
     this.checkRank = function ( xp ){
         this.expirience += xp;
         if ( this.expirience <= 19 ) {
+            this.totalHealth = 30;
             this.rank = "rookie";
             return this.rank;
         } else if ( this.expirience >= 20 && this.expirience <= 49 ) {
             this.rank = "skilled";
+            this.totalHealth = 35;
             return this.rank;
         } else if ( this.expirience >= 50 && this.expirience <= 99 ) {
             this.rank = "veteran";
@@ -56,8 +69,11 @@ function Humanoid ( name ) {
 let leftPlayer = new Humanoid("Soldier");
 let rightPlayer = new Humanoid("Bandit");
 
+
 function Weapon ( name ) {
     this.name = name;
+    this.size = 1;
+    // oneHanded
     this.availability = "rookie";
     this.damage = 3;
     this.accuracy = 0;
@@ -72,6 +88,11 @@ function Armor ( name ) {
     this.chemArmor = 1;
 }
 
+function Inventory ( item ) {
+    this.items= {};
+    // тут методы itemRemove() и item(add) должны пихать и доставать из массива(объекта) размером 9 итемы
+    // сделать проверку if items length == 9 мы блокируем добавление нвых итемов
+}
 // Урон = урон оружия +-модификаторы + dmgRoll( )
 // Точность(выбор КУДА стрелять) = точность оружия +-модификаторы + accRoll( )
 // шанс заклинивания = стандартный шанс заклинивания +- шанс заклинивания используемого оружия(если есть) +-модификторы
